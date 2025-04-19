6804ae4f46e97027bf460ffb142698f3f0163b90
lenox <lenoxmiheso@gmail.com>
2025-04-19 12:42:37 +0300

    feat: created mpesa stk push

---
+++ index.js
@@ -0,0 +1,75 @@
+import express, { response } from "express"
+import dotenv from "dotenv"
+import { getAccessToken } from "./lib/auth.js"
+import { stkPush } from "./lib/stkPush.js"
+
+dotenv.config()
+
+const app = express()
+app.use(express.json())
+
+
+// initiate -stk
+app.post("/initiate", async (req, res) => {
+    try {
+        
+        const {phoneNumber, amount, productName} = req.body;
+        // Databse logic to store transaction details
+        // TABLE: Transaction
+        // Status be pending
+
+        // initiateSTK Push
+        // 1. Get access token
+        const accessToken = await getAccessToken()
+
+        // 2. Initiate Stk push
+        const initiateStkResponse = await stkPush(accessToken, phoneNumber, amount, productName)
+        
+        res.json({
+            success: true,
+            initiateStkResponse
+        })
+
+
+
+
+
+
+    } catch (error) {
+
+        res.status(500).json({
+            success: false, 
+            error: error.message || "Failed to get access tone"
+        })
+        
+    }
+})
+
+// callback endpoint
+app.post('/callback', async (req, res)=> {
+    try {
+        
+        const stkCallbackData = req.body.Body;
+
+        let status = null
+        if (stkCallbackData.ResultCode === 0) {
+            status = "SUCCESS"
+        } else {
+            status = "FAILED"
+        }
+
+        // Database Logic to update the transaction status.
+
+
+
+        res.json({status, stkCallbackData})
+
+
+
+    } catch (error) {
+        res.status(500).json({error: 'something went wrong'})
+    }
+})
+
+
+app.listen(3000, () => console.log("sever started successfully!"))
